1db023f36a5bfe54f605848ad9eadff36b9ddc26
mjanderson09 <michael.j.anderson@intel.com>
2018-10-03 13:51:53 -0700

    remove old bench.sh

--- samples/deeplearning/cnnlayer/compile.sh
+++ samples/deeplearning/cnnlayer/compile.sh
@@ -1,4 +1,3 @@
 cd ../../../
 make realclean && make AVX=3 MIC=0 OMP=1 STATIC=1 -j  
 cd samples/deeplearning/cnnlayer
-make realclean && make AVX=3 MIC=0 OMP=1 STATIC=1 -j 
---
+++ samples/deeplearning/cnnlayer/setup.sh
@@ -0,0 +1,3 @@
+export OMP_NUM_THREADS=28
+export INTEL_LICENSE_FILE=/swtools/intel/licenses/
+source /swtools/intel/compilers_and_libraries_2018/linux/bin/compilervars.sh intel64
